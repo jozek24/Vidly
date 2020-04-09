5003a24a599ad35373c7023bcfabf26999e226a4
Józef Salik <jozek243@gmail.com>
2020-04-09 18:52:52 +0200

    Provide MoviesController to work on database data.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,20 +10,25 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie{Id=1, Name = "Oficer"},
-                new Movie{Id=1, Name = "Trzeci Oficer"}
-            };
-        }
     }
 }
