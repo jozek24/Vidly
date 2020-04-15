928b09d32020233f4bc0b7ce207099e339095995
Józef Salik <jozek243@gmail.com>
2020-04-15 11:49:17 +0200

    Add Display attribute into Movie class.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -12,11 +12,17 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Number In Stock")]
         public byte NumberInStock { get; set; }
         [Required]
         public Genre Genre{ get; set; }
+
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
     }
 }
